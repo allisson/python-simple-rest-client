ebb178afe8c08935c44aaf813550028f533830bc
Danilo Shiga <daniloshiga@gmail.com>
2017-10-02 00:32:11 -0300

    Add client response (#2)
    * Add client_response to Response
    
    * Add client_response reference in quickstart

--- simple_rest_client/models.py
+++ simple_rest_client/models.py
@@ -1,8 +1,8 @@
 from collections import namedtuple
 
 Request = namedtuple(
-    'Request', ['url', 'method', 'params', 'body', 'headers', 'timeout']
+    'Request', ['url', 'method', 'params', 'body', 'headers', 'timeout',]
 )
 Response = namedtuple(
-    'Response', ['url', 'method', 'body', 'headers', 'status_code']
+    'Response', ['url', 'method', 'body', 'headers', 'status_code', 'client_response']
 )
