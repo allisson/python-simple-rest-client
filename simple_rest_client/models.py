6352008a81967e3ddaf50f47a9ff80ce0591404f
Roberto Rodriguez <rfrodriguez@falabella.cl>
2019-06-19 16:38:07 -0400

    Add support to disable certificate verification
    * Add support to disable the certificate verification
    ** This is a very bad practice, but sometimes in develop times is very usefoul

--- simple_rest_client/models.py
+++ simple_rest_client/models.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
 Request = namedtuple(
-    'Request', ['url', 'method', 'params', 'body', 'headers', 'timeout', 'kwargs']
+    'Request', ['url', 'method', 'params', 'body', 'headers', 'timeout', 'verify', 'kwargs']
 )
 Response = namedtuple(
     'Response', ['url', 'method', 'body', 'headers', 'status_code', 'client_response']
