e5815c627a26b4b93ea5ecbece51b51321fe1ffb
Allisson Azevedo <allisson@gmail.com>
2020-01-14 08:05:54 -0300

    Update httpx version

--- simple_rest_client/models.py
+++ simple_rest_client/models.py
@@ -1,6 +1,4 @@
 from collections import namedtuple
 
-Request = namedtuple(
-    "Request", ["url", "method", "params", "body", "headers", "timeout", "ssl_verify", "kwargs"]
-)
+Request = namedtuple("Request", ["url", "method", "params", "body", "headers", "timeout", "kwargs"])
 Response = namedtuple("Response", ["url", "method", "body", "headers", "status_code", "client_response"])
